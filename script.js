66e2e921910067559c6300e64af3cbb3e7ca8290
Semih Metin Zengin <148861856+semihcenk@users.noreply.github.com>
2024-03-15 18:13:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//navbar
+function toggleMenu(){
+    var menuToggle =document.querySelector('.toggle');
+    var menu =document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active')
+}
+// scroll animation: 
+
+const sr= ScrollReveal({
+    origin:'top',
+    distance:'85px',
+    duration:2000,
+    reset:true
+})
+sr.reveal('.home-text', {})
+sr.reveal('.home-img', {delay:200})
+sr.reveal('.social', {delay:200})
+
+sr.reveal('.about-text', {delay:200})
+sr.reveal('.about-img', {})
+
+sr.reveal('.heading', {})
+
+sr.reveal('.servicess-content', {})
+
+sr.reveal('.work-text', {})
+sr.reveal('.work-img', {delay:200})
+
+sr.reveal('.app', {delay:200})
+sr.reveal('.app h1', {})
+
+sr.reveal('form input', {})
+sr.reveal('form textarea', {})
+sr.reveal('.social', {delay:200})
